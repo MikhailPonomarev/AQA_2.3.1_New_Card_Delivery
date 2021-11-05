dd27521bc4ff98c481b704e04475e0cb2682b0f2
MikhailPonomarev <michailp800@gmail.com>
2021-11-05 17:10:16 +0300

    Configured lombok

--- src/test/java/ru/netology/deliverytest/CardDeliveryTest.java
+++ src/test/java/ru/netology/deliverytest/CardDeliveryTest.java
@@ -32,136 +32,26 @@ public class CardDeliveryTest {
                 .shouldHave(exactText("Встреча успешно запланирована на " + meetingDate));
     }
 
-//    @Test
-//    public void changingDeliveryDate() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(3));
-//        $("[data-test-id='name'] input").setValue(user.getName());
-//        $("[data-test-id='phone'] input").setValue(user.getPhone());
-//        $("[data-test-id='agreement']").click();
-//        $(withText("Запланировать")).click();
-//        $("[data-test-id='success-notification'] [class='notification__content']").should(appear)
-//                .shouldHave(exactText("Встреча успешно запланирована на " + deliveryDate(3)));
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(4));
-//        $(withText("Запланировать")).click();
-//        $("[data-test-id='replan-notification']").should(appear);
-//        $("[data-test-id='replan-notification'] .notification__content")
-//                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-//        $(withText("Перепланировать")).click();
-//        $("[data-test-id='success-notification'] [class='notification__content']").should(appear)
-//                .shouldHave(exactText("Встреча успешно запланирована на " + deliveryDate(4)));
-//    }
-//
-//    @Test
-//    public void allFieldsAreEmpty() {
-//        $("[data-test-id='city'] input").setValue("");
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(" ");
-//        $(withText("Мы гарантируем безопасность ваших данных")).click(); //для того чтобы скрыть календарь
-//        $("[data-test-id='name'] input").setValue("");
-//        $("[data-test-id='phone'] input").setValue("");
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Поле обязательно для заполнения")).should(appear);
-//    }
-//
-//    @Test
-//    public void allValuesAreWrong() {
-//        $("[data-test-id='city'] input").setValue("New York");
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(2));
-//        $("[data-test-id='name'] input").setValue("Ivanov Ivan");
-//        $("[data-test-id='phone'] input").setValue("");
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Доставка в выбранный город недоступна")).should(appear);
-//    }
-
-//    @Test
-//    public void emptyCityField() {
-//        $("[data-test-id='city'] input").setValue("");
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue(name());
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Поле обязательно для заполнения")).should(appear);
-//    }
-//
-//    @Test
-//    public void notAllowedCity() {
-//        $("[data-test-id='city'] input").setValue("Реутов");
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue(name());
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Доставка в выбранный город недоступна")).should(appear);
-//    }
-//
-//    @Test
-//    public void emptyNameField() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue("");
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Поле обязательно для заполнения")).should(appear);
-//    }
-//
-//    @Test
-//    public void enteringNameInEnglish() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue("Ivanov Ivan");
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
-//                .should(appear);
-//    }
-//
-//    @Test
-//    public void enteringNameUsingNumbers() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue("123456");
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
-//                .should(appear);
-//    }
-//
-//    @Test
-//    public void enteringNameUsingSpecialChars() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue("%#$@%");
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
-//                .should(appear);
-//    }
-//
-//    @Test
-//    public void emptyTelephoneField() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue(name());
-//        $("[data-test-id='phone'] input").setValue("");
-//        $(".checkbox__box").click();
-//        $(byText("Запланировать")).click();
-//        $(withText("Поле обязательно для заполнения")).should(appear);
-//    }
-//
-//    @Test
-//    public void orderWithNoCheckbox() {
-//        $("[data-test-id='city'] input").setValue(cityGenerate());
-//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate(5));
-//        $("[data-test-id='name'] input").setValue(name());
-//        $("[data-test-id='phone'] input").setValue(phone());
-//        $(byText("Запланировать")).click();
-//        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid']").shouldBe(visible);
-//    }
+    @Test
+    public void changingDeliveryDate() {
+        var validUser = DataGenerator.Registration.generateUser();
+        var firstMeetingDate = DataGenerator.generateDate(5);
+        var secondMeetingDate = DataGenerator.generateDate(10);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='success-notification'] [class='notification__content']").should(appear)
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(secondMeetingDate);
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification']").should(appear);
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $(withText("Перепланировать")).click();
+        $("[data-test-id='success-notification'] [class='notification__content']").should(appear)
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
 }
